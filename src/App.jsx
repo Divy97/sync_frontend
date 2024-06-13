c0bb4510157b97dc8dff344e6952ef0bc84cfe31
Divy <divyparekh1810@gmail.com>
2024-06-13 19:18:12 +0530

    navigation fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate,
 } from "react-router-dom";
 
 import TextEditor from "./components/TextEditor";
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/documents/:id" element={<TextEditor />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
